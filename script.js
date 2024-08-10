96e71c3ed3fbcbb2d58e396dfc69db0c81bb7a66
paryan <pryntl2007@gmail.com>
2024-08-10 14:40:59 +0330

    Update [java script] : comment
    added comments to explain the code.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+//the function base code.
 function number(string, numberOfCharacters) {
+  //made a variable to show the final results.
   let output = 0;
+  //at first removing the spaces (using replaceAll method and replacing each space with nothing.). then extracting the targeted part(using substring method.)
   output = string.replaceAll(" ", "").substr(0, numberOfCharacters);
+  //at the end returning the main var.
   return output;
 }
+//testing.
 console.log(number("this is the string", 6));
